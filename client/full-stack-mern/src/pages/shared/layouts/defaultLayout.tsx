0012608f2a8d37d9b55523fd4fca6eae2936ee95
Sunil <sunil.salaria@lpinfotech.com>
2025-03-06 18:38:57 +0530

    update text

--- client/full-stack-mern/src/pages/shared/layouts/defaultLayout.tsx
+++ client/full-stack-mern/src/pages/shared/layouts/defaultLayout.tsx
@@ -3,7 +3,9 @@ import { Outlet } from 'react-router';
 function DefaultLayout() {
     return (
         <>
+
             <main>
+            <h1>default layout</h1>
                 <Outlet />
             </main>
         </>
